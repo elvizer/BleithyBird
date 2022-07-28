9e8330c2accdc1c66f484f7c0e2cb82a4348fb01
Elvis <gamerprotime777@gmail.com>
2022-07-27 18:08:01 -0700

    IK Manual

---
+++ Test Arms/Assets/Scenes/Arms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class Arms : MonoBehaviour
+{
+    public GameObject rifle;
+    public TwoBoneIKConstraint mover;
+    private GameObject target;
+    private Rifle gun;
+
+    void Awake()
+    {
+        rifle = Instantiate(rifle, gameObject.transform.position, Quaternion.identity);
+        gun = rifle.GetComponent<Rifle>();
+        mover.data.target = gun.target.transform;
+    }
+
+    void Start()
+    {
+       
+    }
+}
